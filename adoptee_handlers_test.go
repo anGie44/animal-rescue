80bb94a9538725e6061237cdcefc78f32e0dd844
Angie Pinilla <apinilla@Angies-MacBook-Pro.local>
2020-03-12 13:26:43 -0400

    cleanup adoptee tests

--- adoptee_handlers_test.go
+++ adoptee_handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -13,16 +14,33 @@ func TestGetAdopteesHandler(t *testing.T) {
 	adoptees := []Adoptee{
 		{1, "Callie", "Australian Shepherd", "female", "puppy"},
 	}
-	// Create Adoptee
+	// CREATE Adoptee
 	form := newCreateAdopteeForm()
-	status := createAdoptee(form)
-
-	if status := recorder.Code; status != http.StatusFound {
-		return err
+	responseRecorder, err := createAdoptee(form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if status := responseRecorder.Code; status != http.StatusFound {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusFound)
 	}
 
-	actual := res[0]
+	// GET Adoptee
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	hf := http.HandlerFunc(getAdopteeHandler)
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	a := []Adoptee{}
+	err = json.NewDecoder(recorder.Body).Decode(&a)
+	actual := a[0]
 	expected := adoptees[0]
 
 	if actual != expected {
@@ -32,36 +50,20 @@ func TestGetAdopteesHandler(t *testing.T) {
 }
 
 func TestCreateAdopteesHandler(t *testing.T) {
-	adoptees = []Adoptee{
-		{1, "Callie", "Australian Shepherd", "female", "puppy"},
-	}
 	form := newCreateAdopteeForm()
-	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
-
+	responseRecorder, err := createAdoptee(form)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
-
-	recorder := httptest.NewRecorder()
-	hf := http.HandlerFunc(createAdopteeHandler)
-	hf.ServeHTTP(recorder, req)
-
-	if status := recorder.Code; status != http.StatusFound {
+	if status := responseRecorder.Code; status != http.StatusFound {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusFound)
 	}
 
-	expected := adoptees[1]
+	a := []Adoptee{}
+	err = json.NewDecoder(responseRecorder.Body).Decode(&a)
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	actual := adoptees[1]
-	if actual != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	if len(a) != 0 {
+		t.Errorf("handler returned unexpected body: got %v want %v", a, []Adoptee{})
 	}
 }
 
@@ -77,7 +79,7 @@ func newCreateAdopteeForm() *url.Values {
 
 }
 
-func createAdoptee(form *url.Values) int {
+func createAdoptee(form *url.Values) (*httptest.ResponseRecorder, error) {
 
 	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
 
@@ -92,6 +94,6 @@ func createAdoptee(form *url.Values) int {
 	hf := http.HandlerFunc(createAdopteeHandler)
 	hf.ServeHTTP(recorder, req)
 
-	return recorder.Code
+	return recorder, nil
 
 }
